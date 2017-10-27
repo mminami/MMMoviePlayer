1717f44ad58b3d863edd36147dd3a96d081be633
mminami <mminami@users.noreply.github.com>
2017-10-27 21:35:16 +0900

    Add MovieItemEntity

---
+++ MMMoviePlayer/MovieItemEntity.swift
@@ -0,0 +1,15 @@
+//
+// Created by mminami on 2017/10/27.
+// Copyright (c) 2017 mminami. All rights reserved.
+//
+
+import Foundation
+
+struct MovieItemEntity {
+    let title: String
+    let presenterName: String
+    let description: String
+    let thumbnailUrlString: String
+    let videoUrlString: String
+    let videoDuration: Int
+}
