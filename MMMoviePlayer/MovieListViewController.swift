956d2294f7cc9395d44d40b428727f61fae478ee
mminami <mminami@users.noreply.github.com>
2017-10-27 21:26:55 +0900

    Add MovieListViewController

---
+++ MMMoviePlayer/MovieListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MovieListViewController.swift
+//  MMMoviePlayer
+//
+//  Created by mminami on 2017/10/27.
+//  Copyright © 2017 mminami. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MovieListViewController: UIViewController {
+    
+}
