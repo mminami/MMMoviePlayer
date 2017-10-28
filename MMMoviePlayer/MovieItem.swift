36dfb99b79744325a984e20fd4a1f118907cc829
mminami <mminami@users.noreply.github.com>
2017-10-28 15:44:52 +0900

    Add MovieItem properties

--- MMMoviePlayer/MovieItem.swift
+++ MMMoviePlayer/MovieItem.swift
@@ -6,5 +6,20 @@
 import Foundation
 
 struct MovieItem {
+    let title: String
+    let videoURL: URL
+    let presenterName: String
+    let description: String
+    let thumbnailURL: URL
+    let videoDuration: Int
 
+    var videoDurationText: String {
+        var components = DateComponents()
+        components.second = videoDuration/1000
+
+        let formatter = DateComponentsFormatter()
+        formatter.zeroFormattingBehavior = .pad
+        formatter.allowedUnits = [.minute, .second]
+        return formatter.string(for: components)!
+    }
 }
