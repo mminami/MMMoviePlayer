2619c349af7bc8c32be363d6fdb80b696e284b15
mminami <mminami@users.noreply.github.com>
2017-10-28 19:12:14 +0900

    Delete ViewController and Main storyboard. Add MovieViewController.

--- MMMoviePlayer/AppDelegate.swift
+++ MMMoviePlayer/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
         let rootVC = MovieListViewController()
         window?.rootViewController = rootVC
         window?.makeKeyAndVisible()
---
+++ MMMoviePlayer/MovieViewController.swift
@@ -0,0 +1,10 @@
+//
+// Created by mminami on 2017/10/28.
+// Copyright (c) 2017 mminami. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MovieViewController: UIViewController {
+}
