4c65a87c2373648273fcbe5d99e331cb46e8f899
mminami <mminami@users.noreply.github.com>
2017-10-28 16:44:58 +0900

    Add configuring function to MovieItemCell

--- MMMoviePlayer/MovieItemCell.swift
+++ MMMoviePlayer/MovieItemCell.swift
@@ -6,13 +6,28 @@
 import Foundation
 import UIKit
 
-class MovieItemCell: UITableViewCell {
+protocol CellProtocol {
+    static var identifier: String { get }
+}
+protocol NibProtocol {
+    static var nib: UINib { get }
+}
+
+class MovieItemCell: UITableViewCell, CellProtocol, NibProtocol  {
+    static var identifier =  "\(MovieItemCell.self)"
+
+    static var nib: UINib {
+        return UINib(nibName: "\(MovieItemCell.self)", bundle: nil)
+    }
+
     @IBOutlet weak var thumbnailImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var durationLabel: UILabel!
 
-    static var nib: UINib {
-        return UINib(nibName: "\(MovieItemCell.self)", bundle: nil)
+    func configure(_ movieItem: MovieItem) {
+        titleLabel.text = movieItem.title
+        descriptionLabel.text = movieItem.description
+        durationLabel.text = movieItem.videoDurationText
     }
 }
